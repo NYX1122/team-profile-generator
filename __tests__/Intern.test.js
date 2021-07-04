464f9a300baebdbc08ab10810ee52399f6294b6a
BoDee <nyx0hemir@gmail.com>
2021-07-03 19:02:14 -0600

    almost done

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,16 @@
+const { test, expect } = require("@jest/globals");
+const Intern = require("../lib/Intern");
+
+test("inherits from employee", () => {
+    const intern = new Intern("BoDee", "12159", "nyx0hemir@gmail.com");
+
+    expect(intern.getName()).toBe("BoDee");
+    expect(intern.getId()).toBe("12159");
+    expect(intern.getEmail()).toBe("nyx0hemir@gmail.com");
+});
+
+test("get school", () => {
+    const intern = new Intern("BoDee", "12159", "nyx0hemir@gmail.com", "Utah State University");
+
+    expect(intern.getSchool()).toBe("Utah State University");
+});
